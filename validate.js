4430ec8e3a731d3ecd8b56d2e893ea3b961f47a8
Timo Hill <tim.hill@theodi.org>
2020-02-05 11:46:21 +0000

    Updating github deployment tasks

--- validate.js
+++ validate.js
@@ -1,1 +1,32 @@
-console.log("hellooo");
+
+const Ajv = require('ajv');
+const fs = require('fs');
+
+var schemafile = "activity-list.json.schema";
+var rawfile = "unvalidated_activity_list.jsonld";
+var goodfile = "validated_activity_list.jsonld";
+
+let schema = JSON.parse(fs.readFileSync(schemafile));
+let data = JSON.parse(fs.readFileSync(rawfile));
+
+var ajv = new Ajv({ allErrors: 'true', verbose: 'true' });
+
+var validate = ajv.compile(schema);
+var is_valid = validate(data);
+
+if(is_valid){
+
+  fs.copyFile(rawfile, goodfile, (err)=>{
+    if(err) throw err;
+    console.log("Validated file successfully copied");
+  });
+
+}
+else{
+
+  err_msg = "File failed validation\n=======\n"
+  console.log(err_msg);
+  console.log(validate.errors);
+  throw new Error(err_msg)
+
+}
